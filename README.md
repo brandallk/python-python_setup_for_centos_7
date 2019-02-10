bcf3f8df252ad6fccb66bbd0a32002204808ec35
Randy King <rking@cradlepoint.com>
2019-02-10 02:54:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+setup.sh is a setup script for installing a Python3 delevoper environment on a CentOS 7 host, e.g. an Amazon t2.micro EC2 using 
+
+To setup the environment, follow these 5 steps:
+1. Create a CloudFormation stack using the ./cloudformationtemplate.yml file in this repo
+2. ssh into the new EC2 using command like this...
+    ssh -i "BrkTrialAws.pem" centos@ec2-3-17-162-158.us-east-2.compute.amazonaws.com
+    ...but look up the public DNS name for the launched EC2 and substitute it for the one in the example above
+3. Open another terminal and scp ./setup.sh into /home/centos/ like so...
+    scp -i "~/.ssh/BrkTrialAws.pem"  ~/code/python/python_setup_for_centos_7/setup.sh centos@ec2-3-17-162-158.us-east-2.compute.amazonaws.com:/home/centos/
+    ...but, again, look up the public DNS name for the launched EC2 and substitute it for the one in the example above
+4. Run command...
+    bash setup.sh
+5. If the /home/centos/src/ folder wasn't created, and the python_scipts repo not cloned into it (final 5 lines of ./setup.sh), run these commands manually.
+
+Save changes to the python_stripts and push the changes back to GitHub.
+
+If something goes wrong, or it's time to terminate the instance, go to CloudFormation and delete the stack.
---
+++ setup.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+sudo yum update -y
+
+# Install packages needed for development and for installing Python
+sudo yum groupinstall -y "development tools"
+sudo yum install -y \
+  lsof \
+  wget \
+  vim-enhanced \
+  words \
+  which \
+  libffi-devel \
+  zlib-devel \
+  bzip2-devel \
+  openssl-devel \
+  ncurses-devel \
+  sqlite-devel \
+  readline-devel \
+  tk-devel \
+  gdbm-devel \
+  db4-devel \
+  libpcap-devel \
+  xz-devel \
+  expat-devel
+
+#  Install Python 3.6
+cd /usr/src
+sudo wget http://python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz
+sudo tar xf Python-3.6.4.tar.xz
+cd Python-3.6.4
+sudo ./configure --enable-optimizations
+# Note: 'make altinstall' will not replace the CentOS built-in python executable (which is required by the OS)
+sudo make altinstall
+
+# Make sure the default secure_path in /etc/sudoers file includes :/usr/local/bin, otherwise cannot run 'sudo pip3.6' command
+sudo sed -i "/secure_path/s/$/:\/usr\/local\/bin/" /etc/sudoers
+# Upgrade pip
+sudo pip3.6 install --upgrade pip
+
+cd /home/centos
+
+# Configure git
+git config --global user.name "Randy King"
+git config --global user.email "randy.king@intimetec.com"
+
+# Customize bash and vim
+curl https://raw.githubusercontent.com/brandallk/content-python3-sysadmin/master/helpers/bashrc -o ~/.bashrc
+exec $SHELL
+curl https://raw.githubusercontent.com/brandallk/content-python3-sysadmin/master/helpers/vimrc -o ~/.vimrc
+
+# Make a /home/centos/src folder, clone the "python_scripts" repo from Github, and checkout its "wip" branch
+# Note: The following lines tend NOT to run when you run the setup.sh script. May have to do this manually.
+mkdir /home/centos/src
+cd /home/centos/src
+git clone https://github.com/brandallk/python_scripts.git
+cd python_scripts
+git checkout wip || git checkout -b wip
